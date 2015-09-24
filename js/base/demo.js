2b20da63c9a65a0db433f66bfe8835b9c830e682
cj85996 <cj85996@alibaba-inc.com>
2015-09-24 10:19:15 +0800

    first commit

---
+++ js/base/demo.js
@@ -0,0 +1,11 @@
+/**
+ * 本demo展示了矩形、线条、圆弧、圆和文本的绘制及属性操作方法
+ */
+define('base/demo', [
+    'jquery'
+    ], function($) {
+    return {
+        init: function () {
+        }
+    };
+});
---
+++ js/main.js
@@ -0,0 +1,7 @@
+require([
+    'base/demo'
+    ], function(base) {
+
+    // 希望查看某一个demo的演示，就取消对应demo模块的注释
+    base.init();      // 基本功能
+});
